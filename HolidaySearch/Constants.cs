9beecca6622246c126fc54e28f342ca24c7b3036
R <richardkemp35@gmail.com>
2024-03-09 00:00:15 +0000

    fixed error in constants Hotels file name

--- HolidaySearch/Constants.cs
+++ HolidaySearch/Constants.cs
@@ -3,5 +3,5 @@ public class Constants
 {
     public const string DATA_DIRECTORY = "Data";
     public const string FLIGHTS_FILE_NAME = "Flights.json";
-    public const string HOTELS_FILE_NAME = "Flights.json";
+    public const string HOTELS_FILE_NAME = "Hotels.json";
 }
