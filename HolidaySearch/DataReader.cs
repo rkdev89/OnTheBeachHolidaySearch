d954086f4a4f144c1390d7ee84aec9a5621a888d
R <richardkemp35@gmail.com>
2024-03-10 23:42:12 +0000

    small refactor

--- HolidaySearch/DataReader.cs
+++ HolidaySearch/DataReader.cs
@@ -3,27 +3,21 @@
 namespace HolidaySearch;
 public class DataReader
 {
-    public static IEnumerable<Flight>? ReadFlightData(string filePath)
-    {   
-        if(!FileExists(filePath)) throw new FileNotFoundException();
-        return ReadJsonFile<IEnumerable<Flight>>(filePath) ?? [];
-    }
+    public static IEnumerable<Flight>? ReadFlightData(string filePath) => ReadJsonFile<IEnumerable<Flight>>(filePath) ?? [];
 
-    public static IEnumerable<Hotel>? ReadHotelData(string filePath)
-    {
-        if (!FileExists(filePath)) throw new FileNotFoundException();
-        return ReadJsonFile<IEnumerable<Hotel>>(filePath) ?? [];
-    }
+    public static IEnumerable<Hotel>? ReadHotelData(string filePath) => ReadJsonFile<IEnumerable<Hotel>>(filePath) ?? [];
+
+    private static bool FileExists(string filePath) => File.Exists(filePath);
 
     public static T? ReadJsonFile<T>(string filePath)
     {
         var json = File.ReadAllText(filePath);
         if (string.IsNullOrEmpty(json)) return default;
+        if (!FileExists(filePath)) throw new FileNotFoundException();
         if(!JsonSchemaValidator.IsValidJson(json)) throw new Exception("Invalid json schema");
 
         var result = JsonConvert.DeserializeObject<T>(json);
         return result;
     }
 
-    private static bool FileExists(string filePath) => File.Exists(filePath);
 }
